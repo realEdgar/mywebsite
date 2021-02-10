bc3385bfb222aeaf83200537492a4fcb977bc051
Edgar Lopez <edgar.lopez.arroyo@hotmail.com>
2021-02-10 14:31:05 -0600

    Finishing the footer and a prototype of main section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
-import {Aside} from './components/Main/Aside'
+import { Main } from './components/Main/Main'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Aside />
+      <Main />
+      <Footer />
     </div>
   );
 }
